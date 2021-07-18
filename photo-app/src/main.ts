55d096a527b933002932f243c65587337077fed4
Leonardo Medina <leo_medina14@hotmail.com>
2021-07-17 22:44:44 -0500

    add project photo-app

---
+++ photo-app/src/main.ts
@@ -0,0 +1,16 @@
+import {User} from './user';
+import { Album } from './album';
+import { Picture } from './picture';
+import { PhotoOrientation } from './photo-orientation';
+
+const user = new User(1, 'luixaviles', 'Luis', true);
+const album = new Album(10, 'Platzi Pictures');
+const picture = new Picture(1, 'TypeScript Course', '2020-03', PhotoOrientation.Landscape);
+// Creamos relaciones
+user.addAlbum(album);
+album.addPicture(picture);
+console.log('user', user);
+
+// Borramos Album
+user.removeAlbum(album);
+console.log('user', user);
